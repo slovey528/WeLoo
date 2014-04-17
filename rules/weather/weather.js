1d9396f4f7d3828022461236f6ad7c20c63a41ce
slovey528 <jiangyumao@gmail.com>
2014-04-16 23:03:57 -0400

    Fetch weather info from World Weather Online

--- rules/weather/weather.js
+++ rules/weather/weather.js
@@ -1,40 +1,42 @@
-var uwapi = require('../../utils/uwapi');
+var weatherapi = require('../../utils/weatherapi');
 var utils = require('../../utils/utils');
 var moment_timezone = require('moment-timezone');
+var celsius = '°C';
+var fahrenheit = '°F';
 
 module.exports = function(webot) {
 	webot.set('current weather',{
   	description:'Use \'weather\' command to get waterloo weather infomation',
-  	pattern: /^(weather|天气)\s*/i,
+  	pattern: /^(weather)\s*/i,
   	handler: function(info, next){
   		info = utils.sanitizeInfo(info);
-
-  		uwapi.getjson('weather/current', function(data) {
-  			if(!utils.isEmptyObject(data)){
-  				var data = data['data'];
-  				var max = data['temperature_24hr_max_c'];
-  				var min = data['temperature_24hr_min_c'];
-  				var now = data['temperature_current_c'];
-          var humidity = data['relative_humidity_percent'];
-          var wind_speed = data['wind_speed_kph'];
-  				var output = utils.localizedText(webot,
-    				{
-      				'en_us' : 'Now: '+now+'°C\tHumidity: +'+humidity+'%\nHighest: '+max+'°C\tLowest: '+min+'°C',
-      				'zh_cn' : '当前气温: '+now+'°C 当前湿度: '+humidity+'%\n最高气温: '+max+'°C 最低气温: '+min+'°C'
-    				});
-  				var reply = {
-  					title: moment_timezone().tz('America/Toronto').format('MMMM Do YYYY, h:mm:ss a'),
-  					url: 'http://www.theweathernetwork.com/weather/canada/ontario/waterloo',
-  					description: output
-  				};
-  			} else{
-   				var output = utils.localizedText(webot, 
-   					{
-        			'en_us' : 'Something wrong... T_T',
-        			'zh_cn' : '天气功能暂时不可用 T_T'
-      			});
-  			}
-  			next(null, reply);
+  		weatherapi.getjson('N2L3G1', function(data) {
+        var data = data['data'];
+        var current = data['current_condition'][0];
+        var cur_temp = current['temp_C']+celsius+' / '+current['temp_F']+fahrenheit;
+        var cur_desc = current['weatherDesc'][0]['value'];
+        var cur_icon = current['weatherIconUrl'][0]['value'].replace('\\', '');
+        var wearther_list = data['weather'];
+        var res = new Array();
+        for (var i = 1; i < wearther_list.length; i++) {
+          (function(i){
+            var today = wearther_list[i]
+            var date = today['date']+':';
+            var max = today['tempMaxC']+celsius+' / '+today['tempMaxF']+fahrenheit;
+            var min = today['tempMinC']+celsius+' / '+today['tempMinF']+fahrenheit;
+            var desc = today['weatherDesc'][0]['value'];
+            var reply = [date+'\t'+desc, 'High: '+max+', '+'Low: '+min].join('\n');
+            res.push(reply);
+          })(i);
+        };
+        var ret = {
+          title: [moment_timezone().tz('America/Toronto').format('MMMM Do YYYY, H:mm:ss'),
+                  'Now: '+cur_temp].join('\n'),
+          pic: cur_icon,
+          url: 'http://www.theweathernetwork.com/weather/canada/ontario/waterloo',
+          description: res.join('\n')
+        };
+		    next(null, ret);
   		});	
   	}
   });
